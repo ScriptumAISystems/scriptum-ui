1e28e9d27a07a5366d3944acb320b58efeab9310
ScriptumAISystems <christian1981.cs41@gmail.com>
2025-05-27 18:54:04 -0700

    Create session.py

---
+++ backend/app/core/backend/app/db/session.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from backend.app.core.config import settings
+
+async_engine = create_async_engine(
+    settings.database_url,
+    echo=settings.debug,
+    future=True,
+)
+
+async_session_maker = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+def get_session() -> AsyncSession:  # pragma: no cover
+    """Yield an async database session."""
+    with async_session_maker() as session:
+        yield session
